0d27feffba2b34383a543e3bcaedc85f33b74914
Splintix98 <40802249+Splintix98@users.noreply.github.com>
2025-05-21 09:03:40 +0200

    add functionality to output config for that port

--- MAC_Finder_DELL_N1500.py
+++ MAC_Finder_DELL_N1500.py
@@ -2,6 +2,14 @@ import paramiko
 import time
 import getpass
 import sys
+import re
+
+
+############# V2 #############
+#
+# Added option to also output the port configuration for the found port which includes VLANs
+#
+##############################
 
 
 ############ TODO ############
@@ -13,10 +21,12 @@ import sys
 
 
 
-# this function is not really in use, it was just a test to get very long outputs to work
-# when the command entered would result in output that is too long to display at once, there would be the option to press enter to get more of the output line by line
-# this function simulates pressing enter until there is no more output available
-# while doing so, as that can take quite a while, it displays sort of a loading indicator
+# This function is not really in use, it was just a test to get very long outputs to work
+# The dell switches truncate output as soon as it gets too long and then you'll have to press enter (possibly multiple times)
+#   to get all of the output.
+# This function implements this enter-pressing exemplary using the "show mac address-table"-command which results in a very long
+#   output showing all mac-addresses known to the switch from the complete network.
+# While collecting the output, a loading indicator is being shown to the user.
 def ssh_test(ip, username, password):
     try:
         ssh_client = paramiko.SSHClient()
@@ -77,8 +87,7 @@ def ssh_test(ip, username, password):
 # execute the command
 # get all the output
 # return the output
-def check_for_mac_on_switch(mac, switch_IP, username, password):
-    # print(f"Checking switch {switch_IP}")
+def exec_ssh_command(command, switch_IP, username, password):
     try:
         ssh_client = paramiko.SSHClient()
         ssh_client.load_system_host_keys()
@@ -95,7 +104,6 @@ def check_for_mac_on_switch(mac, switch_IP, username, password):
         channel.recv(1024)
         # print("Enabled")
 
-        command = "show mac address-table address " + mac + "\n"
         # print(f"command: {command}")
         channel.send(command)
         time.sleep(1)
@@ -113,7 +121,7 @@ def check_for_mac_on_switch(mac, switch_IP, username, password):
     
     except Exception as e:
         return str(e), None
-    
+   
 
 # the Dell Switches need MAC addresses in a specific format (XXXX.XXXX.XXXX) where X can be a digit or an uppercase letter from the range A-F (hex)
 # this function brings the MAC address that the user entered into the correct format for further processing
@@ -132,17 +140,33 @@ def format_mac_address(mac):
         return None
     
 
+# get port ("Gi1/0/32") based on regex pattern
+def get_port_from_output(output):
+    port_regex_pattern = r"\bGi\d+/\d+/\d+\b"  # supports stacks where the port might be "Gi2/0/xx"
+    match = re.search(port_regex_pattern, output)
+    if match:
+        port = match.group(0)
+        print(f"Port found using regex pattern: {port}")
+        return port
+    else:
+        print("No port found in output.")
+        return
+    
+
 # asks for the mac address to search for from the user
 # brings the received mac address in the correct format
 # checks switches one by one
 # if mac is found on current switch:
 #   print the output containing the port and vlan
-#   end the search
-#   ask whether to search again
+#   ask user whether he wants to also see the port configuration
+#   ask user whether the other switches should be searched for the mac regardless of the hit
+# ask whether to search again
 # if mac wasn't found, print the list of switches that were searched
 def check_switches(switch_IPs, password):
     mac = input("Please enter the MAC-address to find: ")
+    device_found = False
         
+    # just for test purposes
     if mac == "test":
         print("192.168.23.31 will now be queried for its MAC-address-table. This can take some time!")
         ssh_test(switch_IPs[0], username, password)
@@ -159,7 +183,8 @@ def check_switches(switch_IPs, password):
     for ip in switch_IPs:
         print(f"Checking switch {ip}...")
         switches_checked.append(ip)
-        output, error = check_for_mac_on_switch(mac, ip, username, password)
+        command = "show mac address-table address " + mac + "\n"
+        output, error = exec_ssh_command(command, ip, username, password)
 
         if error:
             print(f"Error when testing {ip}: ", error)
@@ -169,19 +194,36 @@ def check_switches(switch_IPs, password):
             #   When I got "Forwarding Database Empty" I assumed the device is not present in the network at all
             #   When I got "Po1" I assumed the device is present somewhere, just not on the currently searched switch
             #   Now I simply search all switches until I find the device
-            if not ("Forwarding Database Empty" in output or "Po1" in output):
-                print(f"\n{mac} was found on switch {ip}, see full output:\n{output}")
-                
+            if not ("Forwarding Database Empty" in output or "Po1" in output or "Te1/" in output):
+                print(f"\n{mac} was found on switch {ip}, see full output:\n{output}")  # exemplary output shown at the very bottom
+                device_found = True
+
+                answer = input("\nDo you want to see the interface configuration for that port? [y|n] ")
+                if answer.lower() == "y" or answer.lower() == "yes":
+                    port = get_port_from_output(output) # "Gi1/0/32"
+                    command = "show interfaces switchport " + port + "\n"
+                    output, error = exec_ssh_command(command, ip, username, password)
+                    if error:
+                        print(f"Error when asking for port config of switch {ip}: ", error)
+                    else:
+                        print(output)  # exemplary output shown at the very bottom                        
+
+                if ip != switch_IPs[-1]:
+                    answer = input("\nContinue the search? [y|n] ")
+                    if answer.lower() == "y" or answer.lower() == "yes":
+                        continue
+
                 answer = input("\nSearch again? [y|n] ")
                 if answer.lower() == "y" or answer.lower() == "yes":
                     print("--------------------------------------------------")
                     return 1
                 else: quit()
+                
             # user can add "debug" to the call of the script and then every output of the switch gets printed to the console regardless of whether the device is found or not
             elif "debug" in sys.argv:
                 print(f"Output for switch {ip}:\n{output}")
     
-    print(f"\n{mac} was not found in the network. \nChecked these switches: {switches_checked}")
+    if not device_found: print(f"\n{mac} was not found in the network. \nChecked these switches: {switches_checked}")
     answer = input("\nSearch again? [y|n] ")
     if answer.lower() == "y" or answer.lower() == "yes":
         print("--------------------------------------------------")
@@ -191,7 +233,8 @@ def check_switches(switch_IPs, password):
 
 if __name__ == "__main__":
     # define your switches that you want to be checked here as a string array
-    switch_IPs = ["123.45.67.89", "98.76.54.321"]
+    # die .41 ist der zweite Switch im Dispo-Stack und muss nicht separat geprüft werden!
+    switch_IPs = ["192.168.23.31", "192.168.23.32", "192.168.23.33", "192.168.23.34", "192.168.23.35", "192.168.23.38", "192.168.23.39", "192.168.23.40", "192.168.23.42", "192.168.23.43", "192.168.23.44"]
     port = 22  # Default SSH port
     username = "admin"
 
@@ -202,8 +245,39 @@ if __name__ == "__main__":
         check_switches(switch_IPs, password)
 
 
+
 """
-Output when MAC is directly connected to switch:
+EXEMPLARY OUTPUT:
+show interfaces switchport Gi1/0/32
+
+Port: Gi1/0/32
+VLAN Membership Mode: General Mode
+Member of VLANs : (1722),1010,1030
+Access Mode VLAN: 1 (default)
+General Mode PVID: 1722
+General Mode Ingress Filtering: Enabled
+General Mode Acceptable Frame Type: Admit All
+General Mode Dynamically Added VLANs:
+General Mode Untagged VLANs: 1722
+General Mode Tagged VLANs: 1010,1030
+General Mode Forbidden VLANs:
+Trunking Mode Native VLAN: 1 (default)
+Trunking Mode Native VLAN Tagging: Disabled
+Trunking Mode VLANs Enabled: All
+Private VLAN Host Association: none
+Private VLAN Mapping:
+Private VLAN Operational Bindings:
+Default Priority: 0
+Protected: Disabled
+
+
+dell-n1548p-3#
+"""
+
+
+
+"""
+OUTPUT WHEN MAC IS DIRECTLY CONNECTED TO SWITCH:
 --------------------
 dell-n1548p-1#show mac address-table address 0000.0000.0000
 
@@ -218,7 +292,7 @@ Vlan     Mac Address           Type        Port
 
 
 
-Output when MAC is not directly connected to this switch but apparently known in the network:
+OUTPUT WHEN MAC IS NOT DIRECTLY CONNECTED TO THIS SWITCH BUT APPARENTLY KNOWN IN THE NETWORK:
 --------------------
 dell-n1548p-1#show mac address-table address 0000.0000.0000
 
@@ -233,7 +307,7 @@ Vlan     Mac Address           Type        Port
 
 
 
-Output when MAC is not known to the network according to this switch:
+OUTPUT WHEN MAC IS NOT KNOWN TO THE NETWORK ACCORDING TO THIS SWITCH:
 --------------------
 dell-n1548p-1#show mac address-table address 0000.0000.0000
 
--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # DellN1500_MAC-Finder
-Script that connects with a bunch of Dell N1500 Series networking switches via SSH to find a previously specified MAC address that is connected with the switches.
+Script that connects with a bunch of Dell N1500 Series networking switches via SSH to find the port a device with a previously specified MAC address is connected to.
 
 The script uses python as I wasn't able to get the automated SSH access to work in a powershell script using Kitty or PoshSSH.
 
 I have created an executable using PyInstaller which I would strongly recommend. You'll get a single executable that you can use on any system, regardless of whether it has python or the imported libraries installed as PyInstaller bundles everything up. 
 That executable will then launch a console window where you can comfortably enter the password and the MAC that you're searching for.
+
+How To create executable using PyInstaller:
+    pip install pyinstaller
+	cd path\to\your\script
+	pyinstaller --onefile your_script.py
+	anschließend findet man die generierte .exe unter \path\to\your\script\dist\your_script.exe
 
 
 ![Demo showing the executable generated from the script using PyInstaller](https://github.com/Splintix98/DellN1500_MAC-Finder/blob/main/Demo.png)
